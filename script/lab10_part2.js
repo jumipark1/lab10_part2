d8035ef7675a3a63699f56d2c36859cddbe4c6dd
jumipark1 <parkj71@student.douglascollege.ca>
2022-04-09 23:57:15 -0700

    first commit

---
+++ script/lab10_part2.js
@@ -0,0 +1,14 @@
+var topPosition = 200;
+var leftPosition = -200;
+
+window.onload = function(){
+    animate();
+    }
+    
+    function runPuppy(){
+    var puppy = document.getElementById('puppy');
+    let image = document.createElement('img');
+    image.src = "puppy.jpeg"
+    console.log(image);
+    marquee.appendChild(image);
+    }
